53633f6e32a8d5853bc947ae6cf9c67e23a66b5f
Alex Sologoub <dadeveloper@hotmail.com>
2016-01-07 04:12:30 +0000

    Gmail User Refresh Token Feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
+require 'net/http'
+require 'json'
+
+  
+# Handle ENV Variables 
+id = [Rails.application.secrets.client_id]
+secret = [Rails.application.secrets.client_secret]
+
+
 class User < ActiveRecord::Base
     
   has_many :incoming_messages
+  
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
@@ -12,16 +22,49 @@ class User < ActiveRecord::Base
     credentials = access_token.credentials
     user = User.where(:email => data["email"]).first
   
-     unless user
-         #user = User.create(name: data["name"],
-         user = User.create(
-            email: data["email"],
-            access_token: credentials["token"],
-            refresh_token: credentials["refresh_token"],
-            password: Devise.friendly_token[0,20] 
+    unless user
+       #user = User.create(name: data["name"],
+       user = User.create(
+          email: data["email"],
+          access_token: credentials["token"],
+          refresh_token: credentials["refresh_token"],
+          expires_at: Time.at(credentials["expires_at"]).to_datetime,
+          password: Devise.friendly_token[0,20] 
          )
      end
     user
   end
   
+
+ 
+  def to_params
+    {'refresh_token' => refresh_token,
+    'client_id' => [Rails.application.secrets.client_id],
+    'client_secret' => [Rails.application.secrets.client_secret],
+    'grant_type' => 'refresh_token'}
+  end
+  
+ 
+  def request_token_from_google
+    url = URI("https://accounts.google.com/o/oauth2/token")
+    Net::HTTP.post_form(url, self.to_params)
+  end
+ 
+  def refresh!
+    response = request_token_from_google
+    data = JSON.parse(response.body)
+    update_attributes(
+    access_token: data['access_token'],
+    expires_at: Time.now + (data['expires_in'].to_i).seconds)
+  end
+ 
+  def expired?
+    expires_at < Time.now
+  end
+ 
+  def fresh_token
+    refresh! if expired?
+    access_token
+  end
+
 end
