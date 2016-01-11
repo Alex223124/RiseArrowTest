46dee6c8436d3810330d5efa559d5be36e3b1505
Alex Sologoub <dadeveloper@hotmail.com>
2016-01-11 12:42:50 +0000

    locale full functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,5 +16,9 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[:locale] if params[:locale].present?
   end
+  
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 
 end
